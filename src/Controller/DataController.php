51b6dee8aa34b8c34f00f29ef896ecb8a8e2a3fc
RuslanPeka <35633105+RuslanPeka@users.noreply.github.com>
2021-07-20 21:29:21 +0300

    New Com

--- src/Controller/DataController.php
+++ src/Controller/DataController.php
@@ -8,13 +8,33 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DataController extends AbstractController
 {
+    private $myString = 'Hello, World!';
+
+    public function getMyString()
+    {
+        return $this->myString;
+    }
+
+    public function setMyString(string $newString)
+    {
+        $this->myString = $newString;
+    }
+
+    public function dataToJson($data)
+    {
+        return json_encode($data);
+    }
+
     /**
      * @Route("/data", name="data")
      */
     public function index(): Response
     {
+        $this->myString = $this->dataToJson($this->myString);
+
         return $this->render('data/index.html.twig', [
             'controller_name' => 'DataController',
+            'controller_data' => $this->myString,
         ]);
     }
 }
